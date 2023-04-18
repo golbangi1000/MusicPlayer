1c6f1ff19b1d930c73e12dc538d4b72fcc187a1a
golbangi1000 <kenjames777@naver.com>
2023-04-18 20:54:58 +0900

    musicplayer
    ㅇㅇ

--- Music1/app/src/main/java/com/example/music1/MusicAdapter.kt
+++ Music1/app/src/main/java/com/example/music1/MusicAdapter.kt
@@ -1,5 +1,6 @@
 package com.example.music1
 
+import android.media.MediaPlayer
 import android.net.Uri
 import android.provider.ContactsContract.Data
 import android.view.LayoutInflater
@@ -9,9 +10,11 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.music1.databinding.ItemLayoutBinding
 import java.text.SimpleDateFormat
 
-class MusicAdapter : RecyclerView.Adapter<Holder>() {
+class MusicAdapter : RecyclerView.Adapter<MusicAdapter.Holder>() {
 
     val musicList = mutableListOf<Music>()
+    var mediaPlayer : MediaPlayer? = null
+
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
         val binding = ItemLayoutBinding
@@ -32,29 +35,33 @@ class MusicAdapter : RecyclerView.Adapter<Holder>() {
         holder.setMusic(music)
     }
 
+    inner class Holder(val binding : ItemLayoutBinding) : RecyclerView.ViewHolder(binding.root)    {
+
+        var musicUri: Uri? = null
+
+        init {
+            itemView.setOnClickListener{
+                if(mediaPlayer != null){
+                    mediaPlayer?.release()
+                    mediaPlayer = null
+                }
+                mediaPlayer= MediaPlayer.create(itemView.context, musicUri)
+                mediaPlayer?.start()
+            }
+        }
+
+
+        fun setMusic(music:Music){
+            musicUri = music.getMusicUri()
+            binding.imageAlbum.setImageURI(music.getMusicUri())
+            binding.textArtist.text = music.artist
+            binding.textTItle.text = music.title
+            val sdf = SimpleDateFormat("mm:ss")
+            binding.textDuration.text= sdf.format(music.duration)
+        }
+
+    }
+
 }
 
 
-class Holder(val binding : ItemLayoutBinding) : RecyclerView.ViewHolder(binding.root)    {
-
-    var musicUri: Uri? = null
-
-    init {
-        
-    }
-
-
-    fun setMusic(music:Music){
-        musicUri = music.getMusicUri()
-        binding.imageAlbum.setImageURI(music.getMusicUri())
-        binding.textArtist.text = music.artist
-        binding.textTItle.text = music.title
-        val sdf = SimpleDateFormat("mm:ss")
-        binding.textDuration.text= sdf.format(music.duration)
-
-
-
-
-    }
-
-}
